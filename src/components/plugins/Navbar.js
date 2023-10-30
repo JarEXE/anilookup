77de8249e6e338feb7d9380fcd023f2888d2c2f1
Jarome Eyken <eykenj@gmail.com>
2023-10-30 11:58:48 -0300

    proper navbar component and updated manifest.json

--- src/components/plugins/Navbar.js
+++ src/components/plugins/Navbar.js
@@ -43,6 +43,7 @@ function Navbar({
   const [checked, setChecked] = React.useState(sfwStatus == 1 ? true : false);
   const [user, setUser] = React.useState(null);
   const [closeMenu, setCloseMenu] = React.useState(true);
+  const [screenWidth, setScreenWidth] = React.useState(window.innerWidth);
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -160,92 +161,179 @@ function Navbar({
     });
   }, []);
 
+  React.useEffect(() => {
+    // Update screenWidth state when the window is resized
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array ensures the effect runs once after the initial render
+
   return (
-    <nav className="navbar navbar-default" id="customNav">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <a
-            className="navbar-brand"
-            href="#/"
-            style={customStyles}
-            onClick={() => landingPageRoute()}
-          >
-            AniLookup 🔍
-          </a>
-        </div>
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          <div
-            className="toggle-container"
-            style={{ marginRight: "30px", marginTop: "8px" }}
-          >
-            <div className="toggle-container">
-              <DarkModeToggle onDarkModeToggle={onDarkModeToggle} />
-            </div>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a
+          className="navbar-brand"
+          href="#/"
+          style={customStyles}
+          onClick={() => landingPageRoute()}
+        >
+          AniLookup 🔍
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          style={{
+            backgroundColor: `${isDarkMode ? "#0dcaf0" : "#fff"}`,
+            borderColor: `${isDarkMode ? "#0dcaf0" : "#282828"}`,
+          }}
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <div
+                className="toggle-container"
+                style={{ marginRight: "30px", marginTop: "8px" }}
+              >
+                <div className="toggle-container">
+                  <DarkModeToggle onDarkModeToggle={onDarkModeToggle} />
+                </div>
 
-            <div className="toggle-container">
-              <span>🔞</span>
-              <Switch
-                onChange={handleChange}
-                checked={checked}
-                className="react-switch"
-                height={20}
-                width={46}
-              />
-            </div>
-          </div>
-          {user ? (
-            <CircleMenu
-              startAngle={180}
-              rotationAngle={-120}
-              itemSize={1}
-              radius={5}
-              open={closeMenu ? false : true}
-              rotationAngleInclusive={false}
-              onMenuToggle={(menuActive) =>
-                menuActive === true ? handleMenuOpen() : handleMenuClosed()
-              }
-            >
-              <CircleMenuItem
-                tooltip="Edit Profile"
-                tooltipPlacement={TooltipPlacement.Left}
-                onClick={() => profileRoute()}
-              >
-                <ProfileIcon />
-              </CircleMenuItem>
-              <CircleMenuItem
-                onClick={() => alert("Clicked the item")}
-                tooltip="Lists"
-                tooltipPlacement={TooltipPlacement.Left}
-              >
-                <ListIcon />
-              </CircleMenuItem>
-              <CircleMenuItem
-                onClick={() => handleLogout()}
-                tooltip="Logout"
-                tooltipPlacement={TooltipPlacement.Left}
-              >
-                <ExitIcon />
-              </CircleMenuItem>
-              <CircleMenuItem
-                onClick={() => alert("No info just yet :(")}
-                tooltip="Info"
-                tooltipPlacement={TooltipPlacement.Bottom}
-              >
-                <InfoIcon />
-              </CircleMenuItem>
-            </CircleMenu>
-          ) : (
-            <button
-              className="btn btn-secondary"
-              style={{ borderRadius: "25px" }}
-              onClick={loginRoute}
-            >
-              Login
-            </button>
-          )}
+                <div className="toggle-container">
+                  <span>🔞</span>
+                  <Switch
+                    onChange={handleChange}
+                    checked={checked}
+                    className="react-switch"
+                    height={20}
+                    width={46}
+                  />
+                </div>
+              </div>
+            </li>
+            <hr></hr>
+            <>
+              {user && screenWidth < 992 ? (
+                <>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      href="#/"
+                      onClick={() => profileRoute()}
+                      style={{ color: `${isDarkMode ? "#fff" : "#282828"}` }}
+                    >
+                      Profile
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      href="#/"
+                      onClick={() => alert("Feature coming soon!")}
+                      style={{ color: `${isDarkMode ? "#fff" : "#282828"}` }}
+                    >
+                      Lists
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      href="#/"
+                      onClick={() => alert("No info just yet :(")}
+                      style={{ color: `${isDarkMode ? "#fff" : "#282828"}` }}
+                    >
+                      Info
+                    </a>
+                  </li>
+                  <hr></hr>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      href="#/"
+                      onClick={() => handleLogout()}
+                      style={{ color: `${isDarkMode ? "#fff" : "#282828"}` }}
+                    >
+                      Logout
+                    </a>
+                  </li>
+                  <hr></hr>
+                </>
+              ) : null}
+            </>
+          </ul>
+          <span class="navbar-text">
+            {user && screenWidth > 991 ? (
+              <div className="mobile-width">
+                <CircleMenu
+                  startAngle={180}
+                  rotationAngle={-120}
+                  itemSize={1}
+                  radius={5}
+                  open={closeMenu ? false : true}
+                  rotationAngleInclusive={false}
+                  onMenuToggle={(menuActive) =>
+                    menuActive === true ? handleMenuOpen() : handleMenuClosed()
+                  }
+                >
+                  <CircleMenuItem
+                    tooltip="Edit Profile"
+                    tooltipPlacement={TooltipPlacement.Left}
+                    onClick={() => profileRoute()}
+                  >
+                    <ProfileIcon />
+                  </CircleMenuItem>
+                  <CircleMenuItem
+                    onClick={() => alert("Feature coming soon!")}
+                    tooltip="Lists"
+                    tooltipPlacement={TooltipPlacement.Left}
+                  >
+                    <ListIcon />
+                  </CircleMenuItem>
+                  <CircleMenuItem
+                    onClick={() => handleLogout()}
+                    tooltip="Logout"
+                    tooltipPlacement={TooltipPlacement.Left}
+                  >
+                    <ExitIcon />
+                  </CircleMenuItem>
+                  <CircleMenuItem
+                    onClick={() => alert("No info just yet :(")}
+                    tooltip="Info"
+                    tooltipPlacement={TooltipPlacement.Bottom}
+                  >
+                    <InfoIcon />
+                  </CircleMenuItem>
+                </CircleMenu>
+              </div>
+            ) : !user ? (
+              <>
+                <button
+                  id="loginButton"
+                  className="btn btn-secondary"
+                  style={{ borderRadius: "25px" }}
+                  onClick={loginRoute}
+                >
+                  Login
+                </button>
+              </>
+            ) : null}
+          </span>
+          <Toaster position="top-left" />
         </div>
-
-        <Toaster position="top-left" />
       </div>
     </nav>
   );
