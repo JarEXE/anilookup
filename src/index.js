74be56170ed5fb859d1be54382fb2dfce98f4600
Jarome Eyken <eykenj@gmail.com>
2023-10-30 12:53:19 -0300

    added service worker stuff

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
+import * as serviceWorkerRegistration from "../src/serviceWorkerRegistration";
+import reportWebVitals from "../src/reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+serviceWorkerRegistration.register();
+
+reportWebVitals();
